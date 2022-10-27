d09b6ab4d40ed383e688ba58b441359abb014770
= <=>
2022-10-27 12:42:45 -0500

    added the actual project files

--- src/CurricularAnalyticsDiff.jl
+++ src/CurricularAnalyticsDiff.jl
@@ -1,5 +1,10 @@
 module CurricularAnalyticsDiff
 
 # Write your package code here.
+include("./Diff.jl")
+include("./HelperFns.jl")
+include("./ResultPrint.jl")
+include("./secret_helper.jl")
+include("./Whatif.jl")
 
 end
