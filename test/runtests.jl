a1a350f762517128aa044e62a57e1694f40012ce
= <=>
2022-12-13 15:29:03 -0800

    update file name in test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ using Test
     #UCSD = read_csv("./files/condensed.csv")
     #@test add_course_institutional(UCSD, "MATH 20B.5", 4.0, Dict("MATH 20B" => pre), Dict("MATH 20C" => pre))
 
-    test = read_csv("./files/SY-Curriculum Plan-BE25.csv")
+    test = read_csv("./files/SY-CurriculumPlan-BE25.csv")
     # course from name
     @test course_from_name("MATH 20A", test) === test.courses[2]
 end
