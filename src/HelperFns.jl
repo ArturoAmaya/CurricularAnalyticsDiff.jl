1a4a2300d3b5fbbe2b8d71f7622a13ce401884e2
= <=>
2022-12-15 16:04:21 -0800

    please?

--- src/HelperFns.jl
+++ src/HelperFns.jl
@@ -59,10 +59,10 @@ function get_course_prereqs(course::Course, curriculum::Curriculum)
 end
 
 """
-    course\_from\_name(course_name, curriculum)
+    course_from_name(course_name, curriculum)
 
-Returns the course object with the given course\_name in the given curriculum.
-Serves as a human-readable alternative to course\_from\_id
+Returns the course object with the given course name in the given curriculum.
+Serves as a human-readable alternative to `course_from_id` in the main Curricular Analytics package.
 """
 function course_from_name(course_name::AbstractString, curriculum::Curriculum)
     for c in curriculum.courses
